b9e53653db97d205788742575f37633cc7f9f86d
Kyonax <kyonax25@gmail.com>
2020-08-04 14:57:53 -0500

    Finish | First beta Version

--- TooltipBrowser.lua
+++ TooltipBrowser.lua
@@ -2,15 +2,15 @@
 event = {add = addEventHandler, load = addEvent, execute = triggerServerEvent} -- Make simple calls for addEvent and triggerServerEvent by the variable event
 loadstring(exports.dgs:dgsImportFunction())() -- Load all the functions from the DGS Resource
 const = {
-    position = {X_WINDOW = 0.5, Y_WINDOW = 0.988, NONE = 0},
+    position = {X_WINDOW = 0.5, Y_WINDOW = 0.988, NONE = 0}, -- All the Avaible Positions for the GUI of this Resource
     size = {
         W_WINDOW = 0.25,
         H_WINDOW = 0.12,
         W_BROWSER = 1,
         H_BROWSER = 1,
         TITLE_SIZE = 0
-    },
-    time = {LOAD_DELAY = 300, TOOLTIP_DURATION = 3500},
+    }, -- All the Avaible Sizes for the GUI of this Resource
+    time = {LOAD_DELAY = 300, TOOLTIP_DURATION = 3500, TOOLTIP_STOPWATCH = 6000}, -- Usefull global Timer variables | Delay for Wait, Duration to Stay & StopWatch to Excute
     type = {
         SUCCES = "2",
         WARNING = "3",
@@ -20,8 +20,8 @@ const = {
         "2",
         "3",
         "4"
-    }
-}
+    } -- Types of Tooltips 
+} -- Lua Constructor for easy acces and Variable storage
 --- GUI Init
 URL_PAGE_TOOLTIP = "http://mta/[tooltip]/src/tooltip.html" -- Location of the HTML File (global)
 DGS_WINDOW_TOOLTIP = dgsCreateWindow(const.position.NONE, const.position.NONE,
@@ -44,18 +44,17 @@ dgsSetFont(DGS_WINDOW_TOOLTIP, "default-bold") -- Bold Font (global)
 function createBrowserTooltipGUI()
     dgsSetVisible(DGS_WINDOW_TOOLTIP, true)
     loadBrowserURL(BROWSER_TOOLTIP, URL_PAGE_TOOLTIP)
-end
+end -- Initializing GUI Function | Shows the DGS Window an Load the Root page
 
 function deleteBrowserTooltipGUI()
     if dgsGetVisible(DGS_WINDOW_TOOLTIP) then
         dgsSetVisible(DGS_WINDOW_TOOLTIP, false)
     end
-end -- This function removes the Browser GUI
+end -- Removing GUI Function | Hidde the DGS Window when this is Visible
 
 function sendScriptBrowserTooltip(message, type_tooltip)
     for i = 1, table.getn(const.type) do
         if not i == tonumber(const.type[type_tooltip]) then
-            outputConsole("Removing: " .. i)
             executeBrowserJavascript(BROWSER_TOOLTIP,
                                      "document.querySelector('.error:nth-child(" ..
                                          i .. ")').style = 'display: none'")
@@ -70,7 +69,7 @@ function sendScriptBrowserTooltip(message, type_tooltip)
                                      const.type[type_tooltip] ..
                                      ")').style = 'display: initial'")
     end, const.time.LOAD_DELAY, 1)
-end
+end -- Send a JavaScript instruction to the Browser | This Instruction deletes the previous Tooltips and Show a new one
 
 function showTooltip(message, type_tooltip)
     if dgsGetVisible(DGS_WINDOW_TOOLTIP) then end
@@ -80,7 +79,7 @@ function showTooltip(message, type_tooltip)
             setTimer(function() deleteBrowserTooltipGUI() end,
                      const.time.TOOLTIP_DURATION, 1)
         end
-    end, 6000, 1)
+    end, TOOLTIP_STOPWATCH, 1)
     if not dgsGetVisible(DGS_WINDOW_TOOLTIP) then
         createBrowserTooltipGUI()
         setTimer(function()
@@ -91,7 +90,7 @@ function showTooltip(message, type_tooltip)
                         deleteBrowserTooltipGUI()
                     end, const.time.TOOLTIP_DURATION, 1)
                 end
-            end, 6000, 1)
+            end, TOOLTIP_STOPWATCH, 1)
         end, const.time.LOAD_DELAY, 1)
     end
-end
+end -- Validate all the Posible Window Visible Status and execute the Script
